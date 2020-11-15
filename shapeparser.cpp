af83658bd6e6aa70e44c841ea800d5ef315c47a0
Rthiskidw <reedwilsonmusic@gmail.com>
2020-11-14 21:54:29 -0800

    changing shit

---
+++ shapeparser.cpp
@@ -0,0 +1,6 @@
+#include "shapeparser.h"
+
+shapeParser::shapeParser()
+{
+
+}
---
+++ shapeparser.h
@@ -0,0 +1,11 @@
+#ifndef SHAPEPARSER_H
+#define SHAPEPARSER_H
+
+
+class shapeParser
+{
+public:
+    shapeParser();
+};
+
+#endif // SHAPEPARSER_H
