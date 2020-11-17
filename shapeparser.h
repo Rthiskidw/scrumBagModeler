60b4749f1f0b5a8e55f760c7408c80f81965f432
Dan Mateos <dan.mateos10@yahoo.com>
2020-11-17 01:23:35 -0800

    updated shapeparser.h

--- shapeparser.h
+++ shapeparser.h
@@ -1,11 +1,23 @@
 #ifndef SHAPEPARSER_H
 #define SHAPEPARSER_H
 
+#include <iostream>
+#include <string>
+#include <ios>
+#include <limits>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include "shape.h"
 
 class shapeParser
 {
 public:
-    shapeParser();
+    std::string getStringFromFile(std::ifstream &file);
+
+    shapes* getShapePtr(std::string shapeType);
+
+    void parseShape(std::vector<shapes*>& vShapeList);
 };
 
 #endif // SHAPEPARSER_H
