3383f4518b7b418e3ee3ad4e552d0ce06b6eeebc
Rthiskidw <reedwilsonmusic@gmail.com>
2020-11-17 17:32:25 -0800

    integrating Alan's login screen

--- login.cpp
+++ login.cpp
@@ -1,15 +1,40 @@
 #include "login.h"
 #include "ui_login.h"
+#include <QMessageBox>
+#include <QPixmap>
 
 //making some changes to login
-login::login(QWidget *parent) :
+Login::Login(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::login)
+    ui(new Ui::Login)
 {
     ui->setupUi(this);
 }
 
-login::~login()
+Login::~Login()
 {
     delete ui;
+}
+
+
+//Sign in button clicked , username and password check
+void Login::on_pushButton_clicked()
+{
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_password->text();
+
+    QString uppercaseUsername;
+    uppercaseUsername = username.toUpper();
+
+    if(uppercaseUsername == "A" && password == "A")
+    {
+        QMessageBox::information(this, "Login", "Username and password is correct");
+        hide();
+        MainWindow* mainwindow = new MainWindow(this);
+        mainwindow->show();//since valid usernam and password we can show the main window
+    }
+    else
+    {
+        QMessageBox::warning(this,"Login", "Username and password is not correct");
+    }
 }
