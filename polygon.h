69a3c1908984e78dae1445cf6b9329b6ea1f218b
CraigSpengler <73617928+CraigSpengler@users.noreply.github.com>
2020-12-01 15:14:47 -0800

    Create polygon.h

---
+++ polygon.h
@@ -0,0 +1,53 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+
+#include <stdio.h>
+#include <QPainter>
+#include <QPen>
+#include <QBrush>
+#include <QFont>
+#include <QObject>
+#include <QPaintDevice>
+#include <string>
+
+//using namespace Qt;
+using namespace std;
+
+
+#include "vector.h"
+#include "shape.h"
+
+
+class Polygon : public Shapes
+{
+private:
+
+    Shapes_Vector<QPoint> vertVect;
+    QPoint vertsArray[DEFAULT_VERTICES];
+    int     numVerts;
+
+public:
+
+    Polygon(QPaintDevice* dev = nullptr, int id = -1);
+
+    ~Polygon() override {}
+
+    virtual void draw(QPaintDevice *dev) override;
+
+    virtual void move(int x, int y, int vertex) override;
+
+    double area();
+
+    double perimeter();
+
+    void setNumVertices(int numVertices);
+
+    int getNumVertices()const;
+
+    void addVertex(const QPoint& vertex);
+
+    Shapes_Vector<QPoint>& getVertices();
+};
+
+#endif // POLYGON_H
